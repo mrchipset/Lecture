914c669e2713e55f3d116d0cee094eac5fea1d80
Mr.Chip <mrchip.zxf@outlook.com>
2024-10-21 11:36:52 +0800

    FEAT: enable nav header function

--- header.php
+++ header.php
@@ -52,14 +52,7 @@
             </div>
             <div class="col-mb-12">
                 <nav id="nav-menu" class="clearfix" role="navigation">
-                    <a<?php if ($this->is('index')): ?> class="current"<?php endif; ?>
-                        href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a>
-                    <?php \Widget\Contents\Page\Rows::alloc()->to($pages); ?>
-                    <?php while ($pages->next()): ?>
-                        <a<?php if ($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>
-                            href="<?php $pages->permalink(); ?>"
-                            title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
-                    <?php endwhile; ?>
+                    <?php Typecho_Widget::widget('NavMenu_List')->navMenu('nav'); ?>
                 </nav>
             </div>
         </div><!-- end .row -->
